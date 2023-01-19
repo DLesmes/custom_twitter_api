f0b22f63ac9c4d8676fae2b6efa5b2d26520c4e4
DLesmes <ing.dlesmes@gmail.com>
2023-01-19 00:49:38 -0500

    Initial setting up for API

---
+++ main.py
@@ -0,0 +1,29 @@
+#uvicorn
+import uvicorn
+
+#fastapi
+from fastapi import FastAPI
+from fastapi import status
+
+app = FastAPI()
+
+@app.get(
+    path="/",
+    status_code=status.HTTP_200_OK,
+    tags=["Pages"]
+) # Called Path operation decorator
+def home(): # Called path operation function
+    """
+    Home page of the API
+    This path returns the home page of the API.
+    No parameters are required.
+    """
+    return {"Twitter API'": "Working"}
+
+if __name__ == "__main__":uvicorn.run(
+        "__main__:app",
+        host="localhost",
+        port=8000,
+        reload=True,
+        workers=2
+    )

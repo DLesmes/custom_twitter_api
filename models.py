c664e7ac3d0850bd74c368137455d506003419e2
DLesmes <ing.dlesmes@gmail.com>
2023-01-19 23:52:09 -0500

    users models

--- models.py
+++ models.py
@@ -35,12 +35,23 @@ class UserBase(BaseModel):
 
 
 class UserLogin(UserBase):
-    password: str = Field(..., min_length=8)
+    password: str = Field(
+        ...,
+        min_length=8
+    )
 
 
 class User(UserBase):
-    first_name: str = Field(..., min_length=1, max_length=50)
-    last_name: str = Field(..., min_length=1, max_length=50)
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
     birth_date: Optional[date] = Field(default=None)
 
 
